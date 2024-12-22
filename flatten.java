b0c79a0ccc0f16f4ed0696f10926a4c206bb7c7d
dsinghal05 <divit.singhal@gmail.com>
2024-12-21 19:54:04 -0500

    complete

---
+++ flatten.java
@@ -0,0 +1,26 @@
+public class flatten { //takes a 2 dimensional array and converts it into a 1 dimension array
+    public static void main(String[] args) {
+    
+    
+    int[][] a = {
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11}
+    };
+
+    int rows = a.length;
+    int cols = a[0].length;
+
+    int[] tmp = new int[rows*cols];
+    int z = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            tmp[z] = a[i][j];
+            z++;
+        }
+    }
+    for (int i = 0; i < tmp.length; i++) {
+        System.out.print(tmp[i] + " ");
+    }
+}
+}
